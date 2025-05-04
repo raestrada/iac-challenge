f5bcd9ba0e74eee8bd01df8709dcb84f7ceaaa62
Rodrigo Estrada <rodrigo.estrada@gmail.com>
2025-05-04 11:29:26 -0400

    fix: agregar declaraciones required_version y required_providers para terraform

--- tf-controller/gcp/postgresql/main.tf
+++ tf-controller/gcp/postgresql/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+  
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.75.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
